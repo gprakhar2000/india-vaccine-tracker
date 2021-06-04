155bbaf1f586277583df3059e9c15c6a7c143d6e
Prakhar Gupta <gprakhar2000@gmail.com>
2021-06-04 14:25:28 +0530

    design changes

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -19,21 +19,22 @@ const Mainpage = (props) => {
         return <Tablepage pincode={pincode} />
     }
     else return (
-
-        <div className="container" id="mainpageDiv">
-            <div className="text-center">
-                <div className="text-center">
+        
+        <div className="flag" id="mainpageDiv">
+            <div className="stripe saffron"></div>
+            <div className="stripe py-2 text-center">
+                <div className="m-2 text-center">
                     <div className="d-inline-flex border border-dark" id="headingdiv"><img src={img1} className=" d-inline-block text-center rounded-pill" id="image" /><h2 className="p-2">INDIA <span className="badge badge-primary">Vaccine Tracker</span></h2></div>
                 </div>
-                <form onSubmit={e => onSubmithandler(e, searchsession)}>
-                    <input id="pincode" className="ml-1 mt-2 mb-2 p-2" type="text" placeholder="Enter PinCode.."
+                <form className="text-center" onSubmit={e => onSubmithandler(e, searchsession)}>
+                    <input id="pincode" className="" type="text" placeholder="Enter PinCode.."
                         autoFocus required pattern="[1-9][0-9]{5}" onInvalid={e=>{e.target.setCustomValidity('Please enter valid pincode')}} onInput={e=>{e.target.setCustomValidity('')}} onChange={event => { setSearchSession(event.target.value) }}
                     />
                     
-                    <button type="submit" className="btn btn-outline-dark ml-1 mt-1 mb-2 p-2" id="submit">Submit</button>
+                    <button type="submit" className="btn btn-outline-dark" id="submit">Submit</button>
                 </form>
-
             </div>
+            <div className="stripe green"></div>
         </div>
 
 
--- src/components/tablepage.js
+++ src/components/tablepage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-
+import img2 from './largest-vaccine-banner.jpg'
 import './mainpage.css'
 import { Link } from 'react-router-dom'
 
@@ -43,19 +43,21 @@ const Tablepage = (props) => {
 
         <div>
 
-            <div className="container mt-2">
+            <div className="position-relative container mt-2" id="tablepageDiv">
                 <button className="btn btn-outline-dark d-inline-flex mt-4 p-2 d-flex justify-content-end" onClick={refresh}>Back to Home</button>
-                <div>
+                <div id="availButtons">
+                    
                     <button className="btn btn-danger mr-1 mt-2 mb-2 p-1">NOT AVAILABLE</button>
                     <button className="btn btn-success mr-1 mt-2 mb-2 p-1">AVAILABLE</button>
                 </div>
-                <div class="d-inline-block alert alert-info" role="alert">
+                <img src={img2} className=" position-absolute text-center p-3" id="img2"/>
+                <div className="d-inline-block alert alert-info" role="alert">
                     Showing results for pincode-<span class="badge bg-light text-dark">{props.pincode}</span>
                 </div>
                 <table className="table border shadow table-bordered table-hover table-responsive">
                     <thead className="thead-dark">
                         <tr>
-                            <th >Centre Name</th>
+                            <th className="w-25" id="pehlacol">Centre Name</th>
                             <th >Availability</th>
                         </tr>
                     </thead>
@@ -64,14 +66,15 @@ const Tablepage = (props) => {
                             centers.map((center, index) => (
                                 <tr>
 
-                                    <th className="w-25">{center.name}<br/>
-                                        <span className="font-weight-lighter">{center.address}, <span>{center.district_name}, {center.state_name}</span><br/> </span><span class="badge rounded-pill bg-info text-dark ml-1">{center.fee_type}</span></th>
+                                    <th>{center.name}<br/>
+                                        <span className="font-weight-lighter">{center.address}, {center.district_name}, {center.state_name}</span>
+                                        <span class="badge rounded-pill bg-info text-dark ml-1">{center.fee_type}</span></th>
                                     <td>
                                         {center.sessions.map((session, index) => (
                                             <td><tr className="" ><span className="badge badge-secondary m-1 p-2">{session.date}</span></tr>
                                                 <tr className="d-flex" ><span className="badge badge-primary m-1 p-2">{session.vaccine}</span><span className="badge badge-dark m-1 p-2">Age-{session.min_age_limit}+</span></tr>
-                                                <tr>{session.available_capacity_dose1 > 0 ? <div className="d-flex"><button className="btn btn-success m-1 p-1">Dose1-{session.available_capacity_dose1}</button><a href="https://selfregistration.cowin.gov.in/" target="_blank" className="btn btn-warning m-1 p-1">Book Slot</a></div> : <button className="btn btn-danger m-1 p-1">Dose1 :{session.available_capacity_dose1}</button>}
-                                                    {session.available_capacity_dose2 > 0 ? <div className="d-flex"><button className="btn btn-success m-1 p-1">Dose2-{session.available_capacity_dose2}</button><a href="https://selfregistration.cowin.gov.in/" target="_blank" className="btn btn-warning m-1 p-1">Book Slot</a></div> : <button className="btn btn-danger m-1 p-1">Dose2 :{session.available_capacity_dose2}</button>}
+                                                <tr>{session.available_capacity_dose1 > 0 ? <div className="d-flex"><button className="btn btn-success m-1 p-1">Dose1 :{session.available_capacity_dose1}</button><a href="https://selfregistration.cowin.gov.in/" target="_blank" className="btn btn-warning m-1 p-1">Book Slot</a></div> : <button className="btn btn-danger m-1 p-1">Dose1 :{session.available_capacity_dose1}</button>}
+                                                    {session.available_capacity_dose2 > 0 ? <div className="d-flex"><button className="btn btn-success m-1 p-1">Dose2 :{session.available_capacity_dose2}</button><a href="https://selfregistration.cowin.gov.in/" target="_blank" className="btn btn-warning m-1 p-1">Book Slot</a></div> : <button className="btn btn-danger m-1 p-1">Dose2 :{session.available_capacity_dose2}</button>}
                                                 </tr>
                                             </td>
                                         ))}
